1166db72613a94f500c3489ca0aff7a49afa3739
Rachit Dheri <89472661+RD998@users.noreply.github.com>
2024-07-16 02:52:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function ClientRC() {
         console.log('working');
-        x.src = "https://snware2.pythonanywhere.com/snware/clientRC";
+        x.src = "https://snware1.pythonanywhere.com/snware/clientRC";
         x0.style.color="blue"
         x1.style.color="black"
         x2.style.color="black"
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function SupplyRC() {
       console.log("workin");
-        x.src = "https://snware2.pythonanywhere.com/snware/supplierRC";
+        x.src = "https://snware1.pythonanywhere.com/snware/supplierRC";
         x1.style.color="blue"
         x0.style.color="black"
         x2.style.color="black"
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function DPquote() {
-        x.src = "https://snware2.pythonanywhere.com/snware/DPquote";
+        x.src = "https://snware1.pythonanywhere.com/snware/DPquote";
         x2.style.color="blue"
         x1.style.color="black"
         x0.style.color="black"
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function SPquote() {
-        x.src = "https://snware2.pythonanywhere.com/snware/SPquote";
+        x.src = "https://snware1.pythonanywhere.com/snware/SPquote";
         x3.style.color="blue"
         x1.style.color="black"
         x2.style.color="black"
