fe389bac3c5836f1aec5f1ea9b0801d7250c1f7d
Rachit Dheri <89472661+RD998@users.noreply.github.com>
2024-07-19 12:43:49 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -5,7 +5,7 @@ console.log("hi") ;
 function passwordCheck(){
 userpwd=document.getElementById("pwd").value;
 b1=document.getElementById("status");
-    console.log(userpwd);
+    
     if (userpwd == "snware@4848"){
 window.location= "Home.html";
 
@@ -18,10 +18,10 @@ else{
 }}
 
 function passwordCheck2(){
-    userpwd=document.getElementById("pwd2").value;
+    adminpwd=document.getElementById("pwd2").value;
 b1=document.getElementById("status2");
-    console.log(userpwd);
-    if (userpwd == "admin_login@7866"){
+    
+    if (adminpwd == "admin_login@7866"){
 window.location= "Home.html";
 
     }
